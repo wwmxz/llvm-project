3f82f1ac97748dcff2d65b5ee0034ed55bd93e10
wwmxz <1735325153@qq.com>
2026-03-06 18:49:58 +0800

    Myrisc CH2_6 Code

--- lib/Target/Myrisc/MyriscISelLowering.cpp
+++ lib/Target/Myrisc/MyriscISelLowering.cpp
@@ -40,6 +40,7 @@ MyriscTargetLowering::MyriscTargetLowering(const TargetMachine &TM,
 
   /// 注册合法化的操作
   setOperationAction(ISD::GlobalAddress, MVT::i32, Custom);
+  setOperationAction(ISD::BR_CC, MVT::i32, Expand);
 
   // deirved properties we expose.
   computeRegisterProperties(STI.getRegisterInfo());
